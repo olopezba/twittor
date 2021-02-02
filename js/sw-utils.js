f9c9039fa22e6b23466a1581d1b2ea2b534866c4
olopezba <publi.olb@gmail.com>
2021-02-02 20:10:18 +0100

    primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,17 @@
+
+
+//GUARDAR CACHE DINAMICO
+function actualizarCacheDinamico(dynamicCache,req,res){
+
+
+    if( res.ok){
+        return caches.open(dynamicCache).then(cache=>{
+
+            cache.put(req,res.clone());
+
+            return res.clone();
+        });
+    }else{
+        return res;
+    }
+}
